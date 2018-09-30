90b254e87284a9c4dde084c04e266006c11cd97b
ZiJie Tang <zjtang@CNzjtang.local>
2018-09-30 10:21:45 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Version 1 - Simple Installation##
+v1.1 apt-get update 
+v1.2 Apache
+:w 
+
---
+++ ubuntu_initial_bash.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Update APT Repository
+	echo -e "\033[0;32m Updating Repository \033[0m"
+		apt-get update
+	echo -e "\033[0;32m Repository Updated Sucessfully \033[0m"
+	echo -e "\033[0;32m ======================================================= \033[0m"
+	sleep 3
+# Apache
+	echo -e "\033[0;32m Installing Apache \033[0m"
+		apt-get update
+	echo -e "\033[0;32m Apache Installed Sucessfully \033[0m"
+	echo -e "\033[0;32m ======================================================= \033[0m"
+	sleep 3
+
+
+
+exit 0
